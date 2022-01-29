10e372a31388aa897488c1126ba701a0b97a5d14
Tomasz Heflik <tomasz.heflik@sap.com>
2022-01-30 00:11:03 +0100

    debug

---
+++ keys-generator/main.py
@@ -0,0 +1,13 @@
+"""
+Generate private and public key with works in Azure APIM with validate-jwt
+"""
+import logging
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info("Generate keys")
+
+if __name__ == "__main__":
+    # execute only if run as script
+    main()
